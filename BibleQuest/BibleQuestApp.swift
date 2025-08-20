d2c22a92b2fa1afbbdd886b38a6d25857836389d
Jasmine <marco.lapenna7@hotmail.com>
2025-08-19 22:34:20 -0400

    Initial Commit

---
+++ BibleQuest/BibleQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleQuestApp.swift
+//  BibleQuest
+//
+//  Created by Marco Lapenna on 2025-08-19.
+//
+
+import SwiftUI
+
+@main
+struct BibleQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleQuestTests/BibleQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BibleQuestTests.swift
+//  BibleQuestTests
+//
+//  Created by Marco Lapenna on 2025-08-19.
+//
+
+import Testing
+@testable import BibleQuest
+
+struct BibleQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
